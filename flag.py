b623a60762c1e7108efd923fd8ac06ff672a8ae7
madalex <madal3xcfc5@gmail.com>
2024-11-14 18:05:29 +0300

    a

--- flag.py
+++ flag.py
@@ -1,7 +1,19 @@
-print(f"\u001b[41m{' ' * 20}\u001b[0m")
-for n in range(2):
-    print(f"\u001b[41m{' ' * 8}\u001b[47m{' ' * 4}\u001b[41m{' ' * 8}\u001b[0m")
-print(f"\u001b[41m{' ' * 4}\u001b[47m{' ' * 12}\u001b[41m{' ' * 4}\u001b[0m")
-for n in range(2):
-    print(f"\u001b[41m{' ' * 8}\u001b[47m{' ' * 4}\u001b[41m{' ' * 8}\u001b[0m")
-print(f"\u001b[41m{' ' * 20}\u001b[0m")
+def draw_swiss_flag():
+    red = "\033[41m"   
+    white = "\033[47m" 
+    reset = "\033[0m"  
+    width = 18
+    height = 10
+
+    for i in range(height):
+        if i in [4, 5]:
+            print(red + " " * ((width - 12) // 2) + white + " " * 12 + red + " " * ((width - 12) // 2) + reset)
+        elif i in [2, 3, 6, 7]:
+            print(red + " " * ((width - 4) // 2) + white + " " * 4 + red + " " * ((width - 4) // 2) + reset)
+        else:
+            print(red + " " * width + reset)
+    print("\n")
+
+
+if __name__ == '__main__':
+    draw_swiss_flag()
